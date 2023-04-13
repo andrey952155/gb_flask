aa29fd799aee1b3337f1133852a6c405e8a9763a
andre344 <gveesst@gmail.com>
2023-04-13 15:07:11 +0700

    add product config

--- blog/configs.py
+++ blog/configs.py
@@ -1,3 +1,6 @@
+import os
+
+
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
@@ -18,3 +21,9 @@ class DevConfig(BaseConfig):
 
 class TestingConfig(BaseConfig):
     TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
