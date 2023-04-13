df4b39e881b870416d10e95345df38ca31e5fadd
andre344 <gveesst@gmail.com>
2023-04-13 17:08:43 +0700

    2

--- blog/configs.py
+++ blog/configs.py
@@ -23,7 +23,7 @@ class TestingConfig(BaseConfig):
     TESTING = True
 
 
-# class ProductionConfig(BaseConfig):
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
-#     SECRET_KEY = os.environ.get("SECRET_KEY")
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
